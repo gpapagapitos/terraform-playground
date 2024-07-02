4d5f9db73c4d68f4539146788ff263309eba849c
gpapagapitos <george.papagapitos@slalom.com>
2024-07-02 16:13:49 -0500

    initialize ec2 with script

--- alb/main.tf
+++ alb/main.tf
@@ -2,22 +2,34 @@ provider "aws" {
   region = var.aws_region
 }
 
+data "template_file" "init" {
+  template = file("script.sh")
+}
+
 resource "aws_instance" "instance_1" {
-  ami           = "ami-0cff7528ff583bf9a"
+  ami           = "ami-06c68f701d8090592"
   instance_type = "t2.micro"
 
+  vpc_security_group_ids = ["sg-0c73be0cd1ab583d8"]
+
+  user_data = data.template_file.init.template
+
   tags = {
-    Name      = "Example 1"
+    Name      = "Instance 1"
     Terraform = "true"
   }
 }
 
 resource "aws_instance" "instance_2" {
-  ami           = "ami-0cff7528ff583bf9a"
+  ami           = "ami-06c68f701d8090592"
   instance_type = "t2.micro"
 
+  vpc_security_group_ids = ["sg-0c73be0cd1ab583d8"]
+
+  user_data = data.template_file.init.template
+
   tags = {
-    Name      = "Example 2"
+    Name      = "Instance 2"
     Terraform = "true"
   }
 }
