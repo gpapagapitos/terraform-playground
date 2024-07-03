aebb2434895e09c20f229e4f893dd9180b582e49
gpapagapitos <george.papagapitos@slalom.com>
2024-07-03 16:43:06 -0500

    nlb working

--- nlb/main.tf
+++ nlb/main.tf
@@ -22,6 +22,43 @@ data "aws_subnet" "subnet" {
   id       = each.value
 }
 
+resource "aws_security_group" "nlb_sg" {
+  name        = "demo-sg-nlb"
+  description = "Allow HTTP into NLB"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "launch_wizard_1" {
+  name = "launch-wizard-1"
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.nlb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "instance_1" {
   ami           = "ami-06c68f701d8090592"
   instance_type = "t2.micro"
@@ -50,43 +87,6 @@ resource "aws_instance" "instance_2" {
   }
 }
 
-resource "aws_security_group" "launch_wizard_1" {
-  name = "launch-wizard-1"
-
-  ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "nlb_sg" {
-  name        = "demo-sg-nlb"
-  description = "Allow HTTP into NLB"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "HTTP"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_lb" "nlb" {
   name               = "DemoNLB"
   subnets            = [for subnet in data.aws_subnet.subnet : subnet.id]
@@ -95,11 +95,23 @@ resource "aws_lb" "nlb" {
 }
 
 resource "aws_lb_target_group" "nlb_tg" {
-  name        = "demo-tg-nlb"
-  port        = 80
-  protocol    = "TCP"
-  vpc_id      = data.aws_vpc.default.id
-  target_type = "instance"
+  name                 = "demo-tg-nlb"
+  port                 = 80
+  protocol             = "TCP"
+  vpc_id               = data.aws_vpc.default.id
+  deregistration_delay = 30
+  target_type          = "instance"
+}
+
+resource "aws_lb_listener" "nlb_listener" {
+  load_balancer_arn = aws_lb.nlb.arn
+  port              = 80
+  protocol          = "TCP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.nlb_tg.arn
+    type             = "forward"
+  }
 }
 
 resource "aws_lb_target_group_attachment" "instance_1" {
