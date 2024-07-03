db60993454ecd5772ac6be424910502bbb57bebd
gpapagapitos <george.papagapitos@slalom.com>
2024-07-03 12:01:19 -0500

    only allow access through load balancer

--- alb/main.tf
+++ alb/main.tf
@@ -61,17 +61,10 @@ resource "aws_security_group" "launch_wizard_1" {
   name = "launch-wizard-1"
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
   }
 
   egress {
